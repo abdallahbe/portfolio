ff0ac70b8ac9b335dc560c7ea7d4cb509394313d
abdallahbe <abdallahbe@users.noreply.github.com>
2026-01-29 19:52:37 +0000

    deploy: b47c0970c69cd5dac3ac1bd4d1f874286ddb8121

---
+++ precache-manifest.23e2043eff37507491a2ab26ac01e612.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d924c4db861781a498b2104edd660e3d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e2a89f097712db24e16e",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "da32a7dc0ed89ae30b1b",
+    "url": "/portfolio/static/css/main.c8f6e864.chunk.css"
+  },
+  {
+    "revision": "e2a89f097712db24e16e",
+    "url": "/portfolio/static/js/2.befb7147.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.befb7147.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da32a7dc0ed89ae30b1b",
+    "url": "/portfolio/static/js/main.527920a1.chunk.js"
+  },
+  {
+    "revision": "6c972d9418aa5b4e59ab",
+    "url": "/portfolio/static/js/runtime-main.1b893ad1.js"
+  }
+]);
